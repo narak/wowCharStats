81bb788e62262976107dd436b3b148e77af6b171
Karan Sheth <karan.sheth@nutanix.com>
2021-06-07 09:29:51 +0530

    adds tabs

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import styles from './app.module.css';
 
 import useLocalStorage from '../helpers/useLocalStorage';
 
-import { Layout } from 'antd';
+import { Layout, Menu } from 'antd';
 import AddChar from './AddChar';
 import Char from './Char';
 
 const { Header, Content } = Layout;
 
 function App() {
+    const [tab, setTab] = useLocalStorage('selectedTab', ['1']);
     const [chars, setChars] = useLocalStorage('chars', []);
 
     function onAdd(char) {
@@ -19,20 +20,31 @@ function App() {
         setChars([...chars.slice(0, index), ...chars.slice(index + 1)]);
     }
 
+    function onChangeTab({ keyPath }) {
+        setTab(keyPath);
+    }
+
     return (
-        <div className={styles.app}>
-            <Layout>
-                <Header>
+        <Layout className={styles.app}>
+            <Header>
+                <div className="logo" />
+                <Menu theme="dark" mode="horizontal" selectedKeys={tab} onClick={onChangeTab}>
+                    <Menu.Item key="1">Characters</Menu.Item>
+                    <Menu.Item key="2">Guild Logs</Menu.Item>
+                </Menu>
+            </Header>
+            <Content style={{ padding: '20px 50px' }}>
+                <div className="site-layout-content">
                     <AddChar onAdd={onAdd} />
-                </Header>
-                <Content className={styles.cards}>
-                    {chars &&
-                        chars.map((char, index) => (
-                            <Char key={index} char={char} onDelete={onDelete.bind(this, index)} />
-                        ))}
-                </Content>
-            </Layout>
-        </div>
+                </div>
+            </Content>
+            <Content className={styles.cards}>
+                {chars &&
+                    chars.map((char, index) => (
+                        <Char key={index} char={char} onDelete={onDelete.bind(this, index)} />
+                    ))}
+            </Content>
+        </Layout>
     );
 }
 
