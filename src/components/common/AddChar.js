11f23772f9636e194e0fff24137a25db57ac9bea
Karan Sheth <karan.sheth@nutanix.com>
2021-07-27 12:14:42 +0530

    adds wcl, rio and wow URL adding support

--- src/components/common/AddChar.js
+++ src/components/common/AddChar.js
@@ -1,9 +1,13 @@
 import styles from './addchar.module.css';
 
+import url from 'url';
 import { useState } from 'react';
-import { Button, Input, Select } from 'antd';
+import { Button, Input, Select, Tooltip } from 'antd';
 
 const { Option } = Select;
+
+const NameHelp =
+    'You can paste your WarcraftLogs, Raider.IO or WoW Armory link in here to auto add.';
 
 export default function AddChar({ onAdd }) {
     const [char, setChar] = useState({ region: 'us', server: 'frostmourne', name: '' });
@@ -17,6 +21,28 @@ export default function AddChar({ onAdd }) {
     }
     function onChange(key, e) {
         setChar({ ...char, [key]: e.target.value });
+    }
+    function onChangeName(e) {
+        const name = e.target.value;
+
+        // is url
+        if (/^http.*/i.test(name)) {
+            const { path, hostname } = url.parse(name);
+            const pathBits = path.split('/');
+
+            if (/warcraftlogs/i.test(hostname)) {
+                const [, , region, server, name] = pathBits;
+                onAdd({ region, server, name });
+            } else if (/raider\.io/i.test(hostname)) {
+                const [, , region, server, name] = pathBits;
+                onAdd({ region, server, name });
+            } else if (/worldofwarcraft/i.test(hostname)) {
+                const [, , , region, server, name] = pathBits;
+                onAdd({ region, server, name });
+            }
+        } else {
+            onChange('name', e);
+        }
     }
 
     return (
@@ -44,13 +70,15 @@ export default function AddChar({ onAdd }) {
                 />
             </div>
             <div className={styles.field}>
-                <Input
-                    type="text"
-                    name="char"
-                    value={char.name}
-                    placeholder="Character"
-                    onChange={onChange.bind(this, 'name')}
-                />
+                <Tooltip placement="bottom" title={NameHelp}>
+                    <Input
+                        type="text"
+                        name="char"
+                        value={char.name}
+                        placeholder="Character"
+                        onChange={onChangeName}
+                    />
+                </Tooltip>
             </div>
             <div className={styles.field}>
                 <Button type="primary" htmlType="submit">
